34b72ac45ff5973e96585f765638815f80024f8f
Victor Phelemba <nolochemical@kodemind.com>
2011-02-24 14:16:43 -0500

    two caveats, try threading

--- pyGtk-notifysnippet.py
+++ pyGtk-notifysnippet.py
@@ -12,10 +12,13 @@
 #~ GNU General Public License for more details.
 #~ 
 #~ You should have received a copy of the GNU General Public License
-#~ along with pyRabbit.  If not, see <http://www.gnu.org/licenses/>.
+#~ along with pyGtk-notifiysnippet.  If not, see <http://www.gnu.org/licenses/>.
 import pygtk
 pygtk.require('2.0')
 import gtk
+from threading import Timer
+
+DISPLAY_WAIT_TIME = 3.0 # realistic wait time when it works
 
 class pyGtk_notifiysnippet:
     def delete_event(self, widget, data=None):
@@ -29,18 +32,26 @@ class pyGtk_notifiysnippet:
     def alert_me(self, widget, message):
         self.dialog = gtk.MessageDialog(self.window,gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_INFO, message_format = message)
         self.dialog.move(5000, -5000) # send it to outer space, quadrant 1
-        self.dialog.connect("delete_event", self.delete_event)
-        self.dialog.connect("button_press_event", self.end_alert)
-        self.dialog.show()
-    def end_alert(self, widget):
+        self.dialog.connect("delete_event", self.end_alert)
+        self.dialog.connect("enter_notify_event", self.end_alert)
+        self.dialog.run()
+        '''
+        The interval the timer will wait before executing its 
+        action may not be exactly the same as the interval specified by the user.
+        [http://docs.python.org/library/threading.html]
+        '''
+        # Don't get kicked in the junk, you could be waiting all day, unreliable..
+        self.t = Timer(DISPLAY_WAIT_TIME, self.dialog.destroy()) 
+        self.t.start()
+    def end_alert(self, widget,event,data=None):
         self.statusicon.set_visible(False)
         self.dialog.destroy()
-        
+        self.t.cancel()  # Where only assuming where still waiting
     def __init__(self):
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         self.window.show_all()
 
-        self.alert_user('Zerp!')
+        self.alert_user('Im outta here!!')
         
     def main(self):
         gtk.main()
